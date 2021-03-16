053ad011c88cd970a28a98e15a3562808d153740
ScarGoE <80728147+ScarGoE@users.noreply.github.com>
2021-03-15 20:46:11 -0600

    Add files via upload

---
+++ WebService1.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace C_Areas
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public decimal A_Cuadrado(decimal N1, decimal N2)
+        {
+            calcular c = new calcular();
+            c.Numero1 = N1;
+            c.Numero2 = N2;
+            return c.Area_Cuadrado();
+        }
+        [WebMethod]
+        public decimal A_Circulo(decimal N1)
+        {
+            calcular c = new calcular();
+            return c.Area_Circulo(N1);
+        }
+        [WebMethod]
+        public decimal A_triangulo(decimal N1, decimal N2)
+        {
+            calcular c = new calcular();
+            return c.Area_Triangulo(N1,N2);
+        }
+
+    }
+}
---
+++ calcular.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C_Areas
+{
+    public class calcular
+    {
+        public decimal Numero1 { get; set; }
+        public decimal Numero2 { get; set; }
+        public decimal Area_Cuadrado()
+        {
+            return Numero1 * Numero2;        
+        }
+        public decimal Area_Circulo(decimal N1)
+        {
+            Numero1 = N1;
+            double Pi, N;
+            Pi = 3.1416;
+            N = Convert.ToDouble (Numero1);
+            //Pi = convert.todouble(3.1416);
+            return Convert.ToDecimal(Pi * N * N);
+        }
+        public decimal Area_Triangulo(decimal N1, decimal N2)
+        {
+            Numero1 = N1;
+            Numero2 = N2;
+            var Resultado = ((N1 * N2) / 2);
+            return Resultado;
+        }
+    }
+}
